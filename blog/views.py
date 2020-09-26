7613e3fb6193931017baf573a6dd1cd5cf8e0df2
LaurianoMonte <laurianomonte@gmail.com>
2020-09-26 00:28:39 -0300

    Correção nos bugs dos posts

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,16 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_detail(request, pk):
