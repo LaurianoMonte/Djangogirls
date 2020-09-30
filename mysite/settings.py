38c3ce86298504c0dc07cc61eabb9b52bc138b09
LaurianoMonte <laurianomonte@gmail.com>
2020-09-30 17:41:40 -0300

    'Correçãosettings.py'

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,5 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 
-de_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
